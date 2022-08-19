55e09cff6de1755c2133b00fbf6fc33c5a851d90
j8sh <j8shigarcia@aol.com>
2022-08-18 21:27:48 -0500

    Updates

---
+++ precache-manifest.e74487d9ec3beadef48b105601c68ccf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ef1654b9bdc42978b31cc949aeb940",
+    "url": "/Estrus-Synchronization-Planner/index.html"
+  },
+  {
+    "revision": "5f106bae7ebbf2a7826d",
+    "url": "/Estrus-Synchronization-Planner/static/css/2.53fe27fa.chunk.css"
+  },
+  {
+    "revision": "68f748468d63539109df",
+    "url": "/Estrus-Synchronization-Planner/static/css/main.0229c9f4.chunk.css"
+  },
+  {
+    "revision": "5f106bae7ebbf2a7826d",
+    "url": "/Estrus-Synchronization-Planner/static/js/2.a0abe148.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Estrus-Synchronization-Planner/static/js/2.a0abe148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f748468d63539109df",
+    "url": "/Estrus-Synchronization-Planner/static/js/main.f6cc0459.chunk.js"
+  },
+  {
+    "revision": "e2b0af47db2bc63eebc6",
+    "url": "/Estrus-Synchronization-Planner/static/js/runtime-main.60c76741.js"
+  },
+  {
+    "revision": "eb03b4ae2e1245a90322882c2e266e0a",
+    "url": "/Estrus-Synchronization-Planner/static/media/bp.eb03b4ae.png"
+  },
+  {
+    "revision": "6eee08a46cb4b0dc4e87b045a0dab074",
+    "url": "/Estrus-Synchronization-Planner/static/media/brtf_full-color.6eee08a4.png"
+  },
+  {
+    "revision": "d0bb69d2767ce505d67582570673c136",
+    "url": "/Estrus-Synchronization-Planner/static/media/hp.d0bb69d2.png"
+  },
+  {
+    "revision": "d13d6e3cefa96196b43027b1f7838e8d",
+    "url": "/Estrus-Synchronization-Planner/static/media/ssp.d13d6e3c.png"
+  }
+]);
