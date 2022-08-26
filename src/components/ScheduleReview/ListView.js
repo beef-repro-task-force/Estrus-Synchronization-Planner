9f707753b581f4ff3061ea45f6fb47e867462ac7
Josh Garcia <jgarcia@kedzie-30-78.dhcp.ksu.edu>
2022-08-26 10:42:41 -0500

    making progress on changing error numbers in json file to correct times

--- src/components/ScheduleReview/ListView.js
+++ src/components/ScheduleReview/ListView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import "./ListView.css"
+import { dateTimePickerDefaultProps } from '@material-ui/pickers/constants/prop-types';
 
 const ListView = props => {
     const { ListOfInstrucitons, DateToStartBreeding, SynchronizationProtocol, GNRH, PG} = props;
@@ -11,25 +12,39 @@ const ListView = props => {
     console.log(DateToStartBreeding.getHours());
     //console.log(DateToStartBreeding.getDate());
 
+    // fixing some timing issues
     const length = Object.keys(ListOfInstrucitons).length;
-    for(var i = 0; i < length; i ++){
-        
-        if(JSON.stringify(ListOfInstrucitons[i].step2) != undefined){
+    var minusErr;
+    var plusErr;
+    var marginOfErr;
+    var tmpDate1 = DateToStartBreeding;
+    var tmpDate2 = DateToStartBreeding;
+    for(var i = 0; i < length; i ++){   
+        if(JSON.stringify(ListOfInstrucitons[i].step2) !== undefined){
+            
             if(JSON.stringify(ListOfInstrucitons[i].step2).includes("between the hours") ){
-                console.log("this should work now!");
-                var minusError = 24 * parseInt(JSON.stringify(ListOfInstrucitons[i].step3));
-                var plusError = 24 * parseInt(JSON.stringify(ListOfInstrucitons[i].step4));
+                if( JSON.stringify(ListOfInstrucitons[i].step3).includes("0.166666667") ){
+                    minusErr = 24 * 0.166666667;
+                }
+                if( JSON.stringify(ListOfInstrucitons[i].step4).includes("0.666666667") ){
+                    plusErr = 24 * 0.666666667;
+                }
 
-                var marginOfErr = (minusError + plusError)/2;
+                console.log(minusErr);
+                console.log(plusErr);
                 
-                ListOfInstrucitons[i].step3 = (DateToStartBreeding.getHours - marginOfErr).toString;
-                ListOfInstrucitons[i].step4 = (DateToStartBreeding.getHours + marginOfErr).toString; 
+                marginOfErr = (plusErr - minusErr)/2;
 
+                tmpDate1.setHours(tmpDate1.getHours()-marginOfErr);
+                tmpDate2.setHours(tmpDate2.getHours()+marginOfErr);
+                console.log(tmpDate1);
+                console.log(marginOfErr);
+                console.log(tmpDate1.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }));
+                ListOfInstrucitons[i].step3 = tmpDate1.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+                ListOfInstrucitons[i].step4 = tmpDate2.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
                 console.log(JSON.stringify(ListOfInstrucitons[i].step3));
-                console.log(JSON.stringify(ListOfInstrucitons[i].step4));
             }
         }
-        
     }
 
     const leadingZeroesHours = (DateObject) =>{
