384bb138807638259fa2e494fbe608ef9d0e9885
Cristian Miclea <>
2025-04-27 16:08:45 +0300

    Refactor App
    Fixed calendar protocols being used
    Removed dead code
    Fixed formatting
    Refactored structure
    Removed unused variables and fixed all warnings
    Locked packages versions as newer ones will prevent the app from running

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
-import Header from "./components/header";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
-import './style/App.css';
+import Header from "./components/Layout/Header";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import "./style/App.css";
 
 function App() {
   return (
     <>
-    <Header />
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
 
-    <Footer />
+      <Footer />
     </>
   );
 }
--- src/components/validProtocols.js
+++ src/components/validProtocols.js
@@ -1,168 +1,181 @@
-import {React, useEffect} from "react";
+import { React, useEffect } from "react";
 import { Button, ButtonGroup } from "@mui/material";
-//import swal from 'sweetalert';
 import ProtocolData from "../components/Protocols.json";
 
-import '../style/validProtocols.css';
+import "../style/validProtocols.css";
 
-const ValidProtocols = props => {
-    const {
-        UserFlow, setUserFlow, 
-        BreedType, CowOrHeifer, SemenType,
-        SystemType, setSynchronizationProtocol,
-        preferList, setPreferList, lessPreferList, setLessPreferList
-    } = props;
+const ValidProtocols = (props) => {
+  const {
+    UserFlow,
+    setUserFlow,
+    BreedType,
+    CowOrHeifer,
+    SemenType,
+    SystemType,
+    setSynchronizationProtocol,
+    preferList,
+    setPreferList,
+    lessPreferList,
+    setLessPreferList,
+  } = props;
 
-    // create a rules engine and grab the rules
-    const { Engine } = require('json-rules-engine');
-    const engineRules = require('../components/test-rules.json');
+  // create a rules engine and grab the rules
+  const { Engine } = require("json-rules-engine");
+  const engineRules = require("../components/test-rules.json");
+  var SynchProtocolTitleData = ProtocolData.Protocols[0];
+  let selectedProtocolHeader = "";
 
-    var SynchProtocolTitleData = ProtocolData.Protocols[0];
-    let selectedProtocolHeader = "";  
-    
-    // object variable to hold the user's inputs and compare it to the rules
-    const factInput = {
-        "BreedType" : BreedType,
-        "SemenType" : SemenType,
-        "SystemType": SystemType
-    }  
+  // object variable to hold the user's inputs and compare it to the rules
+  const factInput = {
+    BreedType: BreedType,
+    SemenType: SemenType,
+    SystemType: SystemType,
+  };
 
-    switch(SystemType){
-        case "Estrus AI":
-            selectedProtocolHeader = "Heat Detect & Breed";
-            break;
-        case "Estrus AI + Clean-up AI":
-            selectedProtocolHeader = "Heat Detect & Clean-up AI";
-            break;
-        case "Fixed-Time AI":
-            selectedProtocolHeader = "Fixed-Time AI";
-            break;
-        case "Split Time AI":
-            selectedProtocolHeader = "Split Time AI";
-            break;
-        default: 
-            break;
-    }
+  switch (SystemType) {
+    case "Estrus AI":
+      selectedProtocolHeader = "Heat Detect & Breed";
+      break;
+    case "Estrus AI + Clean-up AI":
+      selectedProtocolHeader = "Heat Detect & Clean-up AI";
+      break;
+    case "Fixed-Time AI":
+      selectedProtocolHeader = "Fixed-Time AI";
+      break;
+    case "Split Time AI":
+      selectedProtocolHeader = "Split Time AI";
+      break;
+    default:
+      break;
+  }
 
-    // this is for the prefer systems
-    useEffect(() => {
+  // this is for the prefer systems
+  useEffect(() => {
+    const fetchData1 = async () => {
+      let protocolsArr = [];
 
-        const fetchData1 = async () => {
+      for (
+        let i = 1;
+        i <=
+        Object.keys(engineRules[`${CowOrHeifer} Preferred Systems`]).length;
+        i++
+      ) {
+        let engine = new Engine();
+        engineRules[`${CowOrHeifer} Preferred Systems`][i].forEach((item) => {
+          engine.addRule(item);
+        });
 
-            let protocolsArr = [];
+        const data = await engine.run(factInput);
 
-                for(let i = 1; i <= Object.keys(engineRules[`${CowOrHeifer} Preferred Systems`]).length; i ++){
-                    let engine = new Engine();
-                    engineRules[`${CowOrHeifer} Preferred Systems`][i].forEach(item => {
-                        engine.addRule(item);
-                    });
+        data.events.forEach((item) => {
+          protocolsArr.push(item.type);
+        });
+      }
+      setPreferList(protocolsArr);
+    };
 
-                    const data = await engine.run(factInput);
+    fetchData1().catch(console.error);
+  }, []);
 
-                    
-                    data.events.forEach((item) => {
-                        //console.log(item);
-                        //setPreferList([...preferList, item.type]);
-                        protocolsArr.push(item.type);
-                    });
-                    
-                } 
-                //console.log("protocolsArr " , protocolsArr);
-                setPreferList(protocolsArr);
-            }  
+  // this is for the less preferr systems
+  useEffect(() => {
+    const fetchData1 = async () => {
+      let protocolsArr = [];
 
-            fetchData1().catch(console.error);
-    }, [])
+      for (
+        let i = 1;
+        i <=
+        Object.keys(engineRules[`${CowOrHeifer} Less Preferred Systems`])
+          .length;
+        i++
+      ) {
+        let engine = new Engine();
+        engineRules[`${CowOrHeifer} Less Preferred Systems`][i].forEach(
+          (item) => {
+            engine.addRule(item);
+          }
+        );
+        const data = await engine.run(factInput);
 
-    // this is for the less preferr systems
-    useEffect(() => {
-        
-        const fetchData1 = async () => {
+        data.events.forEach((item) => {
+          protocolsArr.push(item.type);
+        });
+      }
+      setLessPreferList(protocolsArr);
+    };
 
-            let protocolsArr = [];
+    fetchData1().catch(console.error);
+  }, []);
 
-            for(let i = 1; i <= Object.keys(engineRules[`${CowOrHeifer} Less Preferred Systems`]).length; i ++){
-                let engine = new Engine();
-                engineRules[`${CowOrHeifer} Less Preferred Systems`][i].forEach(item => {
-                    engine.addRule(item);
-                });
+  return (
+    <div className="protocol-div-container">
+      <br />
+      <br />
+      <div>
+        <h2>
+          {" "}
+          {selectedProtocolHeader} - {CowOrHeifer} Protocols{" "}
+        </h2>
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical contained button group"
+          variant="text"
+        >
+          {preferList.map((item, index) => {
+            if (item !== "") {
+              return (
+                <Button
+                  key={index}
+                  onClick={() => {
+                    setSynchronizationProtocol(item);
+                    setUserFlow(UserFlow + 1);
+                  }}
+                >
+                  ({item}){" "}
+                  {SynchProtocolTitleData[item].SynchronizationSystemTitle}
+                </Button>
+              );
+            }
+          })}
+        </ButtonGroup>
 
-                const data = await engine.run(factInput);
-
-                
-                data.events.forEach((item) => {
-                    //console.log(item);
-                    //setPreferList([...preferList, item.type]);
-                    protocolsArr.push(item.type);
-                });
-                
-            } 
-            setLessPreferList(protocolsArr);
-        }  
-
-            fetchData1().catch(console.error);
-    }, [])
-
-
-    return(
-        <div className='protocol-div-container'>
-            <br />
-            <br />
-            <div>
-            <h2> {selectedProtocolHeader} - {CowOrHeifer} Protocols </h2>
-            <ButtonGroup
-            orientation="vertical"
-            aria-label="vertical contained button group"
-            variant="text"
-            >
-            {preferList.map((item, index) => {
-                if(item !== ""){ 
-                    return(
-                        <Button 
-                        key={index} 
-                        onClick={() => {setSynchronizationProtocol(item); setUserFlow(UserFlow +1)}}
-                        > 
-                            ({item}) {SynchProtocolTitleData[item].SynchronizationSystemTitle} 
-                        </Button>
-                    )
-                }})}
-            </ButtonGroup>
-
-           
-            <h2> Less Preferred Systems </h2>
-            <ButtonGroup
-            orientation="vertical"
-            aria-label="vertical contained button group"
-            variant="text"
-            >
-                {lessPreferList.map((item, index) => {
-                    if(item !== "" ){
-                        return(
-                            <Button 
-                            key={index} 
-                            onClick={() => {setSynchronizationProtocol(item);setUserFlow(UserFlow +1)}}
-                            > 
-                                ({item}) {SynchProtocolTitleData[item].SynchronizationSystemTitle} 
-                            </Button>
-                        )
-                    }})}
-                
-            </ButtonGroup>
-            </div>
-                <br />
-            <Button 
-                onClick = {() => {setUserFlow(UserFlow -1)}}
-                variant   = "outlined" 
-                size      = "large"
-                className='custom-btn-styling-stuff'
-            >
-            Return
-            </Button>
-
-        </div>
-        
-    );
-
-}
+        <h2> Less Preferred Systems </h2>
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical contained button group"
+          variant="text"
+        >
+          {lessPreferList.map((item, index) => {
+            if (item !== "") {
+              return (
+                <Button
+                  key={index}
+                  onClick={() => {
+                    setSynchronizationProtocol(item);
+                    setUserFlow(UserFlow + 1);
+                  }}
+                >
+                  ({item}){" "}
+                  {SynchProtocolTitleData[item].SynchronizationSystemTitle}
+                </Button>
+              );
+            }
+          })}
+        </ButtonGroup>
+      </div>
+      <br />
+      <Button
+        onClick={() => {
+          setUserFlow(UserFlow - 1);
+        }}
+        variant="outlined"
+        size="large"
+        className="custom-btn-styling-stuff"
+      >
+        Return
+      </Button>
+    </div>
+  );
+};
 
 export default ValidProtocols;
